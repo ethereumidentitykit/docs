fbd5bd85a4b2ad655b202d2184161d4d9940c2e8
encryptedDegen <encrypted@ethfollow.xyz>
2025-03-24 21:52:36 +0100

    add back api

---
+++ pages/docs/api/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  index: 'Introduction',
+  users: 'Users',
+  lists: 'Lists',
+  leaderboard: 'Leaderboard',
+  token: 'Token',
+  stats: 'Stats',
+  discover: 'Discover',
+  'export-state': 'Export State',
+  swagger: 'Swagger',
+}
